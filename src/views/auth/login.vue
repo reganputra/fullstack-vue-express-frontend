0bf3458fd637f490a8f8ddf85ab0e47f8a26bf2b
reganputra <ramadhanregan334@gmail.com>
2025-01-30 19:43:04 +0700

    refactor: remove wording

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -6,7 +6,7 @@
   <div class="p-5 mb-4 bg-light rounded-3 shadow-sm">
     <div class="container-fluid py-5">
       <h1 class="display-5 fw-bold">HALAMAN LOGIN</h1>
-      <p class="col-md-12 fs-4">Belajar Full Stack JavaScript Developer dengan Express dan Vue 3 di SantriKoding.com</p>
+      <p class="col-md-12 fs-4">Belajar Full Stack JavaScript Developer dengan Express dan Vue 3 </p>
     </div>
   </div>
 </template>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -6,7 +6,7 @@
 <div class="p-5 mb-4 bg-light rounded-3 shadow-sm">
   <div class="container-fluid py-5">
     <h1 class="display-5 fw-bold">FULLTSACK JAVASCRIPT DEVELOPER</h1>
-    <p class="col-md-12 fs-4">Belajar Full Stack JavaScript Developer dengan Express dan Vue 3 di SantriKoding.com</p>
+    <p class="col-md-12 fs-4">Belajar Full Stack JavaScript Developer dengan Express dan Vue 3 di </p>
     <hr/>
     <router-link :to="{ name: 'register' }" class="btn btn-primary btn-lg me-3">REGISTER</router-link>
     <router-link :to="{ name: 'login' }" class="btn btn-secondary btn-lg">LOGIN</router-link>
@@ -14,4 +14,3 @@
 </div>
 </template>
 
->
