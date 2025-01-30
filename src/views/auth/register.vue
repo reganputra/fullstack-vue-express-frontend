ca0ad67f6d691d7effec32283faaf886c13931df
reganputra <ramadhanregan334@gmail.com>
2025-01-30 19:41:54 +0700

    feat: add register and login pages

---
+++ src/views/auth/register.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="p-5 mb-4 bg-light rounded-3 shadow-sm">
+    <div class="container-fluid py-5">
+      <h1 class="display-5 fw-bold">HALAMAN REGISTER</h1>
+      <p class="col-md-12 fs-4">Belajar Full Stack JavaScript Developer dengan Express dan Vue 3 di SantriKoding.com</p>
+    </div>
+  </div>
+</template>
+
