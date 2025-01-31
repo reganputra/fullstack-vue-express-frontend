70a5da4f913fdcb4bdb1fa0dcbb72039c74e5b7d
reganputra <ramadhanregan334@gmail.com>
2025-01-31 08:00:44 +0700

    feat: admin dashboard

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,12 @@
 
 import { createRouter, createWebHistory } from 'vue-router'
 
+import Cookies from "js-cookie";
+
+// get token
+const getToken = () => Cookies.get('token')
+
+
 //define routes
 const routes = [
     {
@@ -17,6 +23,12 @@ const routes = [
         path: '/login',
         name: 'login',
         component: () => import('../views/auth/login.vue')
+    },
+    {
+        path: '/admin/dashboard',
+        name: 'dashboard',
+        component: () => import('../views/admin/admin.vue'),
+        meta: { requiresAuth: true }
     }
 ]
 
@@ -25,5 +37,27 @@ const router = createRouter({
     history: createWebHistory(),
     routes
 })
+
+//Global route
+router.beforeEach((to, from, next) => {
+    // Ambil token otentikasi pengguna
+    const token = getToken();
+
+    // Jika rute tujuan membutuhkan otentikasi dan pengguna tidak memiliki token
+    if (to.matched.some(record => record.meta.requiresAuth) && !token) {
+        // Alihkan pengguna ke halaman login
+        next({ name: 'login' });
+    }
+    // Jika rute tujuan adalah halaman login atau register dan pengguna sudah login
+    else if ((to.name === 'login' || to.name === 'register') && token) {
+        // Alihkan pengguna ke halaman dashboard
+        next({ name: 'dashboard' });
+    }
+    // Jika tidak ada kondisi khusus, izinkan navigasi ke rute tujuan
+    else {
+        next();
+    }
+});
+
 
 export default router
---
+++ src/views/admin/admin.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+  import SidebarMenu from "../../components/SidebarMenu.vue";
+</script>
+
+<template>
+  <div class="container mt-5 mb-5">
+    <div class="row">
+      <div class="col-md-3">
+        <SidebarMenu />
+      </div>
+      <div class="col-md-9">
+        <div class="card border-0 rounded shadow-sm">
+          <div class="card-header">
+            DASHBOARD
+          </div>
+          <div class="card-body">
+            Selamat Datang, <strong></strong>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
