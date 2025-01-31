18c4c60617847873f28fc29e36c0640f25f984c9
reganputra <ramadhanregan334@gmail.com>
2025-01-31 07:38:58 +0700

    refactor: minor change

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a href="https://santrikoding.com" target="_blank" class="nav-link active" aria-current="page">SANTRIKODING.COM</a>
+              <a href="https://reganputra.github.io/portofolio/" target="_blank" class="nav-link active" aria-current="page">Regan Belajar</a>
             </li>
           </ul>
         </div>
