2b5cc0969fe98077819267ca82c0029c69ed0924
reganputra <ramadhanregan334@gmail.com>
2025-04-16 16:24:22 +0700

    refactor: chang backend url

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://fullstack-vue-express-backend.vercel.app/api",
 })
 
 export default Api;
